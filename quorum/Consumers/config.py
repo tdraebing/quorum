f8a13dec0ddc513ab7abae6b6839c1eed0d18488
alarcj <alarcj137@gmail.com>
2017-05-31 15:33:59 -0400

    initial lib struct

---
+++ quorum/Consumers/config.py
@@ -0,0 +1,16 @@
+consumer_key    = 'sFSeQNdkcLCEPUi81p4ClN3kC'
+consumer_secret = 'O9tec0ETYkyn0p1km6hYUQa2akkz9tlB6QAMu2nLOij0UbNBRa' 
+access_token    = '265463749-7uUERK4V6BZT8lEhVKlCTJfHOWfCUxEJaYuI3ysj'
+access_secret   = 'jWuskeWJTs8DxSSXgsQlXPPXajVrkqVqN6HNm2n3pnVWI'
+
+
+consumer_key    = 'bqvwgyk8I949tn3lJiHlgGfqk'
+consumer_secret = 'yif4sIXhLHoGCQmBiu8AUoqiY2WcgTaHO44QQnfbQVuLuf40SG'
+access_token    = '265463749-0dR3F7mUcuU2PM0uTxR4sQWaODHFRYzDEauJH27z'
+access_secret   = 'lWBFfYdqxu9tK5s626hSDpx1tvkUCOpPW1Cvk1tLlyyHq'
+
+consumer_key    = 'sFSeQNdkcLCEPUi81p4ClN3kC'
+consumer_secret = 'O9tec0ETYkyn0p1km6hYUQa2akkz9tlB6QAMu2nLOij0UbNBRa'          
+access_token    = '265463749-7uUERK4V6BZT8lEhVKlCTJfHOWfCUxEJaYuI3ysj'          
+access_secret   = 'jWuskeWJTs8DxSSXgsQlXPPXajVrkqVqN6HNm2n3pnVWI'
+
