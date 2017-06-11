eabb0249c832faa066d5f16d96a3b3c6ab66bebf
alarcj <alarcj137@gmail.com>
2017-06-11 09:13:04 -0400

    modified test

--- DOCKER_SETUP/scraper_test/RUN_TEST.sh
+++ DOCKER_SETUP/scraper_test/RUN_TEST.sh
@@ -6,14 +6,13 @@ basedir=`pwd`
 cd ../
 bash install_docker.sh |& tee INSTALL_DOCKER.txt
 
-cd ../Dockerfiles
+cd Dockerfiles
 sudo docker build -t ubuntu_miniconda -f Dockerfile_miniconda . |& tee MINICONDA_IMAGE.txt
 sudo docker build -t selenium -f Dockerfile_selenium . |& tee SELENIUM_IMAGE.txt
 
 cd ../../
-cp ${basedir}/Dockerfile_ddw .
-cp ${basedir}/dataworld.py .
-sudo docker build -t ddw -f Dockerfile_ddw . |& tee SCRAPER_IMAGE.txt
+dockerfile=${basedir}/Dockerfile_ddw
+sudo docker build -t ddw -f $dockerfile . |& tee SCRAPER_IMAGE.txt
 sudo docker run -it -v $(pwd)/scraper_data:/code/data ddw
 
 popd
