66e232a93dc34842790a3ef5c8d10cfed20e021b
alarcj <alarcj137@gmail.com>
2017-06-12 12:53:01 -0400

    commenting out docker's installation

--- DOCKER_SETUP/scraper_test/RUN_TEST.sh
+++ DOCKER_SETUP/scraper_test/RUN_TEST.sh
@@ -4,15 +4,15 @@ pushd .
 basedir=`pwd`
 
 cd ../
-bash install_docker.sh |& tee INSTALL_DOCKER.txt
+#bash install_docker.sh |& tee INSTALL_DOCKER.txt
 
 cd Dockerfiles
 sudo docker build -t ubuntu_miniconda -f Dockerfile_miniconda . |& tee MINICONDA_IMAGE.txt
 sudo docker build -t selenium -f Dockerfile_selenium . |& tee SELENIUM_IMAGE.txt
 
 cd ../../
-dockerfile=${basedir}/Dockerfile_ddw
-sudo docker build -t ddw -f $dockerfile . |& tee SCRAPER_IMAGE.txt
-sudo docker run -it -v $(pwd)/scraper_data:/code/data ddw
+cp ${basedir}/Dockerfile_ddw .
+sudo docker build -t ddw -f Dockerfile_ddw . |& tee SCRAPER_IMAGE.txt
+sudo docker run -it -v $(pwd)/scraper_data:/code/data -m 6g ddw
 
 popd
