4da0b8d3f5749d35437495575b9646dc4dd7b4e3
alarcj <alarcj137@gmail.com>
2017-06-14 15:41:55 -0400

    added more functionality to manage topics

--- quorum/utils/kafka_utils.py
+++ quorum/utils/kafka_utils.py
@@ -20,16 +20,25 @@ def terminate_producer(topic):
     producer = KafkaProducer(bootstrap_servers='localhost:9092')                
     producer.send(topic, signal_msg.encode('utf-8'))
 
-def generate_id(length):                                                        
+def generate_id(length=6):                                                        
     choices = string.ascii_lowercase + string.ascii_uppercase + string.digits   
     return ''.join(random.choices(choices, k=length))                           
                                                                                 
-def generate_n_ids(n, length):                                                  
-    ids = set()                                                                 
+def generate_n_ids(n, length=6, ids=[]):                                                  
+    ids = set(ids)                                                                 
     while len(ids)!=n and n>0:                                                  
         ids.add(generate_id(length))                                            
     return list(ids)                                                            
-                                                                                
+
+def generate_topic():
+    existing_topics = existing_topics()
+    while True:
+        new_topic = generate_id()
+        if new_topic not in existing_topics:
+            create_kafka_topics([topic])
+            return new_topic
+
+
 def existing_topics():
     work_path = os.getcwd()                                                     
     kafka_path = work_path+'/KAFKA/kafka_2.11-0.10.2.0'
@@ -37,7 +46,7 @@ def existing_topics():
     topics = subprocess.run(list_topics, stdout=subprocess.PIPE)
     return topics.stdout.decode('utf-8').split('\n')
 
-def create_kafka_topics(topics):                                                
+def create_kafka_topics(topics):   
     work_path = os.getcwd()                                                     
     kafka_path = work_path+'/KAFKA/kafka_2.11-0.10.2.0'                                                                                                     
     create_topic = '/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic '
